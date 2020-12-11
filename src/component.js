623e98e1f304056b2068e75a514cba8a6841db2c
maxilozoz <2802146520@qq.com>
2020-12-11 13:05:36 +0800

    package

--- src/component.js
+++ src/component.js
@@ -1,3 +1,8 @@
 import maxiloVue from 'maxilo-vue'
 import antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
 maxiloVue.vue.use(antd)
+
+import avt from 'antd-vue-tool'
+import 'antd-vue-tool/dist/index.css'
+maxiloVue.vue.use(avt)
